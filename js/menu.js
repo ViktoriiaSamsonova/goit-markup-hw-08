c862fafcc66064c48a377483427b2143d6a5ecd3
ViktoriiaSamsonova <112159351+ViktoriiaSamsonova@users.noreply.github.com>
2022-11-30 15:19:11 +0200

    hw_08

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,12 @@
       closeMenuBtn: document.querySelector("[data-menu-close]"),
       menu: document.querySelector("[data-menu]"),
       body: document.querySelector("body"),
-      menuList: document.querySelector("mob-menu__list"),
+      menuList: document.querySelector("mob-menu"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menuList.addEventListener("click", removeMenu);
 
   function toggleMenu() {
       refs.menu.classList.toggle("is-hidden");
